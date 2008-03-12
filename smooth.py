a4eba86834282064a31a3ecee4a463b8e4a537d5
Jakub Wilk <jwilk@jwilk.net>
2008-03-12 23:31:35 +0000

    Rendering.

--- smooth.py
+++ smooth.py
@@ -22,7 +22,62 @@ class OpenDialog(wx.FileDialog):
 
 	def __init__(self, parent):
 		wx.FileDialog.__init__(self, parent, style = wx.OPEN, wildcard = 'DjVu files (*.djvu, *.djv)|*.djvu;*.djv|All files|*')
-            
+
+PIXEL_FORMAT = djvu.decode.PixelFormatRgb()
+PIXEL_FORMAT.rows_top_to_bottom = 1
+PIXEL_FORMAT.y_top_to_bottom = 1
+
+class PageWidget(wx.Panel):
+
+	def __init__(self, *args, **kwargs):
+		wx.Panel.__init__(self, *args, **kwargs)
+		self.page_job = None
+		self.Bind(wx.EVT_PAINT, self.on_paint)
+		self.Bind(wx.EVT_SIZE, self.on_size)
+		self.on_size(None)
+	
+	def on_size(self, event):
+		self.width, self.height = self.GetClientSizeTuple()
+		self._buffer = wx.EmptyBitmap(self.width, self.height)
+		self.update_drawing()
+	
+	def on_paint(self, event):
+		wx.BufferedPaintDC(self, self._buffer)
+
+	def update_drawing(self):
+		page_job = self.page_job
+		my_width, my_height = self.width, self.height
+		dc = wx.BufferedDC(wx.ClientDC(self), self._buffer)
+		try:
+			if page_job is None:
+				raise djvu.decode.NotAvailable
+			page_width, page_height = page_job.width, page_job.height
+			data = page_job.render(
+				djvu.decode.RENDER_COLOR,
+				(0, 0, my_width, my_height),
+				(0, 0, my_width, my_height),
+				PIXEL_FORMAT,
+				1
+			)
+			image = wx.EmptyImage(my_width, my_height)
+			image.SetData(data)
+			dc.DrawBitmap(image.ConvertToBitmap(), 0, 0)
+			self._buffer = image.ConvertToBitmap()
+		except djvu.decode.NotAvailable, ex:
+			N = 16
+			dc.Clear()
+			dc.SetBrush(wx.Brush((0x80, 0x80, 0x80)))
+			dc.SetPen(wx.Pen((0x80, 0x80, 0x80)))
+			for y in xrange((self.height + N - 1) // N):
+				for x in xrange((self.width + N - 1) // N):
+					if (x ^ y) & 1:
+						continue
+					dc.DrawRectangle(x * N, y * N, N, N)
+	
+	def set_page_job(self, page_job):
+		self.page_job = page_job
+		self.on_size(None)
+
 class MainWindow(wx.Frame):
 	
 	def new_menu_item(self, menu, text, help, method, icon = None):
@@ -39,6 +94,7 @@ class MainWindow(wx.Frame):
 		self.Connect(-1, -1, wx.EVT_DJVU_MESSAGE, self.handle_message)
 		self.context = Context(self)
 		self.base_title = 'DjVuSmooth'
+		self.page_widget = PageWidget(self)
 		self.do_open(None)
 		if not __debug__:
 			sys.excepthook = self.except_hook
@@ -76,7 +132,15 @@ class MainWindow(wx.Frame):
 		else:
 			self.document = self.context.new_document(djvu.decode.FileURI(path))
 		self.update_title()
+		self.update_page_widget()
 	
+	def update_page_widget(self, page_job = None):
+		if self.document is None:
+			page_job = None
+		elif page_job is None:
+			page_job = self.document.pages[0].decode(wait = False)
+		self.page_widget.set_page_job(page_job)
+
 	def update_title(self):
 		if self.path is None:
 			title = self.base_title
@@ -96,6 +160,8 @@ class MainWindow(wx.Frame):
 			print 'IGNORED',
 		print self, message
 		self.update_title()
+		if isinstance(message, djvu.decode.ChunkMessage) and message.page_job is not None:
+			self.update_page_widget(message.page_job)
 
 class Context(djvu.decode.Context):
 
