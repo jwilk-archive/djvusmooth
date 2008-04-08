455774e05212b0aeda45d71fa8dcf03588639a26
Jakub Wilk <jwilk@jwilk.net>
2008-04-08 22:05:59 +0000

    external_editor: initial import.

---
+++ external_editor.py
@@ -0,0 +1,18 @@
+# encoding=UTF-8
+# Copyright © 2008 Jakub Wilk <ubanus@users.sf.net>
+
+import os.path
+import subprocess
+
+def edit(file_name):
+	file_name = os.path.abspath(file_name)
+	edit = subprocess.Popen(
+		['edit', 'text/plain:%s' % file_name],
+		stdin = subprocess.PIPE,
+		stdout = subprocess.PIPE
+	)
+	edit.stdin.close()
+	edit.stdout.close()
+	edit.wait()
+
+# vim:ts=4 sw=4
