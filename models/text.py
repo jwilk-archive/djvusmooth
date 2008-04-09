951fb594b08f3d93a920761ebd147f5440d4f5c1
Jakub Wilk <jwilk@jwilk.net>
2008-04-09 11:06:09 +0000

    models.text: add `Text` and `PageText` classes.

--- models/text.py
+++ models/text.py
@@ -4,6 +4,44 @@
 
 import djvu.sexpr
 
+from models import MultiPageModel
+
+class Text(MultiPageModel):
+
+	def get_page_model_class(self, n):
+		return PageText
+
+class PageText(object):
+
+	def __init__(self, n, original_data):
+		self._sexpr = self._original_sexpr = orignal_data
+		self._dirty = False
+		self._n = n
+
+	@apply
+	def value()
+		def get(self)
+			return self._sexpr
+		def set(self, value):
+			self._sexpr = value
+			self._dirty = True
+		return property(get, set)
+	
+	def clone(self):
+		from copy import copy
+		return copy(self)
+	
+	def export(self, djvused):
+		djvused.select(self._n + 1)
+		djvused.set_text(self._sexpr)
+	
+	def revert(self):
+		self._sexpr = self._original_sexpr
+		self._dirty = False
+	
+	def is_dirty(self):
+		return self._dirty
+	
 def extract_text(sexpr):
 	if len(sexpr) < 5:
 		pass # should not happen
