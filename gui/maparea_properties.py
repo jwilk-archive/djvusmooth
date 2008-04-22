78364861bdf4c8fb421317bb4f0a27370963cc94
Jakub Wilk <jwilk@jwilk.net>
2008-04-22 20:33:20 +0000

    gui.maparea_properties: remove that silly static box; add label/edit for comment.

--- gui/maparea_properties.py
+++ gui/maparea_properties.py
@@ -10,17 +10,16 @@ class MapareaPropertiesDialog(wx.Dialog):
 	def __init__(self, parent):
 		wx.Dialog.__init__(self, parent, title = 'Overprinted annotations properites')
 		sizer = wx.BoxSizer(wx.VERTICAL)
-		uri_box = wx.StaticBox(self, label = 'URI and target')
-		uri_box_sizer = wx.StaticBoxSizer(uri_box, wx.VERTICAL)
-		uri_box_gs =wx.FlexGridSizer(2, 2, 5, 5)
-		uri_box_sizer.Add(uri_box_gs)
+		gs_sizer = wx.FlexGridSizer(3, 2, 5, 5)
 		uri_label = wx.StaticText(self, label = 'URI:')
 		uri_edit = wx.TextCtrl(self, size = (self.DEFAULT_TEXT_WIDTH, -1))
 		target_label = wx.StaticText(self, label = 'Target frame:')
 		target_edit = wx.TextCtrl(self, size = (self.DEFAULT_TEXT_WIDTH, -1))
-		for widget in uri_label, uri_edit, target_label, target_edit:
-			uri_box_gs.Add(widget, 0)
-		sizer.Add(uri_box_sizer, 1, wx.EXPAND | wx.ALL, 5)
+		comment_label = wx.StaticText(self, label = 'Comment:')
+		comment_edit = wx.TextCtrl(self, size = (self.DEFAULT_TEXT_WIDTH, -1))
+		for widget in uri_label, uri_edit, target_label, target_edit, comment_label, comment_edit:
+			gs_sizer.Add(widget, 0)
+		sizer.Add(gs_sizer, 1, wx.EXPAND | wx.ALL, 5)
 		button_sizer = wx.StdDialogButtonSizer()
 		button = wx.Button(self, wx.ID_OK)
 		button.SetDefault()
