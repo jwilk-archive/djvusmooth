77cbebbbe5aa13ee660d2c12343289fe82da2398
Jakub Wilk <jwilk@jwilk.net>
2008-04-22 21:22:53 +0000

    gui.maparea_properties: add again silly static boxen.

--- gui/maparea_properties.py
+++ gui/maparea_properties.py
@@ -18,7 +18,9 @@ class MapareaPropertiesDialog(wx.Dialog):
 	def __init__(self, parent):
 		wx.Dialog.__init__(self, parent, title = 'Overprinted annotations properites')
 		sizer = wx.BoxSizer(wx.VERTICAL)
-		gs_sizer = wx.FlexGridSizer(3, 2, 5, 5)
+		main_properties_box = wx.StaticBox(self, label = 'Main properties')
+		main_properties_box_sizer = wx.StaticBoxSizer(main_properties_box)
+		gs_sizer = wx.FlexGridSizer(0, 2, 5, 5)
 		uri_label = wx.StaticText(self, label = 'URI:')
 		uri_edit = wx.TextCtrl(self, size = (self.DEFAULT_TEXT_WIDTH, -1))
 		target_label = wx.StaticText(self, label = 'Target frame:')
@@ -31,7 +33,11 @@ class MapareaPropertiesDialog(wx.Dialog):
 		comment_edit = wx.TextCtrl(self, size = (self.DEFAULT_TEXT_WIDTH, -1))
 		for widget in uri_label, uri_edit, target_label, target_edit, comment_label, comment_edit:
 			gs_sizer.Add(widget, 0)
-		sizer.Add(gs_sizer, 1, wx.EXPAND | wx.ALL, 5)
+		main_properties_box_sizer.Add(gs_sizer, 0, wx.EXPAND | wx.ALL, 5)
+		sizer.Add(main_properties_box_sizer, 0, wx.EXPAND | wx.ALL, 5)
+		border_box = wx.StaticBox(self, label = 'Border')
+		border_box_sizer = wx.StaticBoxSizer(border_box)
+		sizer.Add(border_box_sizer, 0, wx.EXPAND | wx.ALL, 5)
 		line = wx.StaticLine(self, -1, style = wx.LI_HORIZONTAL)
 		sizer.Add(line, 0, wx.EXPAND | wx.BOTTOM | wx.TOP, 5)
 		button_sizer = wx.StdDialogButtonSizer()
