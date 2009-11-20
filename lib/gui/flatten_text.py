398a4a23f99c20fae171add18f6c5fdef67f55b6
Jakub Wilk <jwilk@jwilk.net>
2009-11-20 21:55:53 +0100

    Update copyright information.

--- lib/gui/flatten_text.py
+++ lib/gui/flatten_text.py
@@ -1,5 +1,6 @@
 # encoding=UTF-8
 # Copyright © 2008, 2009 Jakub Wilk <ubanus@users.sf.net>
+# Copyright © 2009 Mateusz Turcza <mturcza@mimuw.edu.pl>
 #
 # This package is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
