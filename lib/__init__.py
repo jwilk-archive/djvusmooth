a8e5449ef834ea34e3d69661d4e306418c8e66fd
Jakub Wilk <jwilk@jwilk.net>
2013-02-18 23:42:26 +0100

    Check Python version at runtime.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,11 @@
 __version__ = '0.2.14'
 __author__ = 'Jakub Wilk <jwilk@jwilk.net>'
+
+import sys
+
+if sys.version_info < (2, 5):
+    raise RuntimeError('Python >= 2.5 is required')
+if sys.version_info >= (3, 0):
+    raise RuntimeError('Python 2.X is required')
+
+# vim:ts=4 sw=4 et
