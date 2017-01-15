88d9f6b05554343deac386e1ea4c5001437a8d4f
Jakub Wilk <jwilk@jwilk.net>
2017-01-15 17:45:47 +0100

    Move __version__ and __author__ downwards.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,11 @@
-__version__ = '0.2.19'
-__author__ = 'Jakub Wilk <jwilk@jwilk.net>'
-
 import sys
 
 if sys.version_info < (2, 6):
     raise RuntimeError('Python >= 2.6 is required')
 if sys.version_info >= (3, 0):
     raise RuntimeError('Python 2.X is required')
+
+__version__ = '0.2.19'
+__author__ = 'Jakub Wilk <jwilk@jwilk.net>'
 
 # vim:ts=4 sts=4 sw=4 et
