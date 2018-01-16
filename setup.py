a47eb52e285c80b4210161a4b2373f09891f358e
Jakub Wilk <jwilk@jwilk.net>
2018-01-16 13:33:51 +0100

    setup: simplify regexp.

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ class build_doc(distutils_build):
             )
             # Use RFC 3339 date format:
             contents = self._date_regex.sub(
-                lambda m: r'{year}\-{month}\-{day}'.format(**m.groupdict()),
+                lambda m: '{year}-{month}-{day}'.format(**m.groupdict()),
                 contents
             )
             file.seek(0)
