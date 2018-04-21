jwilk-archive/djvusmooth

commit a0c36f5c8e51a296236c619ec478d326ece47f99

setup: get version from changelog.

setup.py
@@ -36,14 +36,17 @@ except ImportError:
 else:
     distutils644.install()
 
-from lib import __version__
-
 data_files = []
 
 if os.name == 'posix':
     data_files += [
         ('share/applications', ['extra/djvusmooth.desktop'])
     ]
+
+def get_version():
+    with open('doc/changelog', 'r') as file:
+        line = file.readline()
+    return line.split()[1].strip('()')
 
 class build_mo(distutils_build):
 
@@ -195,7 +198,7 @@ Topic :: Multimedia :: Graphics
 
 distutils.core.setup(
     name='djvusmooth',
-    version=__version__,
+    version=get_version(),
     license='GNU GPL 2',
     description='graphical editor for DjVu',
     long_description=__doc__.strip(),

commit 3220c8d9364bd7626dbde440b3f9b759df234426

setup: add Python 3 version check.
Fixes: a0c36f5c8e51a296236c619ec478d326ece47f99

setup.py
@@ -18,6 +18,7 @@
 '''
 
 b''  # Python >= 2.6 is required
+exec b''  # Python 2.X is required
 
 import glob
 import os