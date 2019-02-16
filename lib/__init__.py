0aa0b6ae516a1bc5edd123e605ce3173e4c82079
Jakub Wilk <jwilk@jwilk.net>
2019-02-16 14:37:51 +0100

    Bump version to 0.3.1.

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,7 +5,7 @@ if sys.version_info < (2, 6):
 if sys.version_info >= (3, 0):
     raise RuntimeError('Python 2.X is required')
 
-__version__ = '0.3'
+__version__ = '0.3.1'
 __author__ = 'Jakub Wilk <jwilk@jwilk.net>'
 
 # vim:ts=4 sts=4 sw=4 et
